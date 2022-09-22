9fe87bc9685998ea6635738d7a1247c8213f4afc
joalucass <114157586+joalucass@users.noreply.github.com>
2022-09-22 09:38:17 -0300

    Atualizar 'main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -1,3 +1,28 @@
+input.onPinPressed(TouchPin.P0, function () {
+    basic.showNumber(3)
+    basic.showNumber(2)
+    basic.showNumber(1)
+    basic.showString("go")
+    basic.pause(randint(1000, 5000))
+    basic.showLeds(`
+        # # . # #
+        # . # . #
+        . # . # .
+        # . # . #
+        # # . # #
+        `)
+    basic.pause(1000)
+    basic.clearScreen()
+})
+input.onPinPressed(TouchPin.P1, function () {
+    basic.showLeds(`
+        . . # # #
+        . . # # #
+        . . # # #
+        . . . # #
+        . . . # #
+        `)
+})
 basic.forever(function () {
 	
 })
